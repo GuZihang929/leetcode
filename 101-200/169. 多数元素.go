adad8969603eb6d8e4484ad3d97067add0a22d34
GuHang <499809640@qq.com>
2023-10-11 10:02:32 +0800

    这个使用的是map记录，没有用到数组是有序的，可以使用双指针

--- 101-200/169. 多数元素.go
+++ 101-200/169. 多数元素.go
@@ -1,5 +1,27 @@
 package main
 
+import "sort"
+
 func main() {
+
+}
+
+func majorityElement(nums []int) int {
+	if len(nums) < 2 {
+		return nums[0]
+	}
+	sort.Ints(nums)
+
+	slow, fast := 0, 1
+
+	for fast-slow < len(nums)/2 {
+		if nums[slow] == nums[fast] {
+			fast++
+		} else {
+			slow = fast
+			fast++
+		}
+	}
+	return nums[fast]
 
 }
