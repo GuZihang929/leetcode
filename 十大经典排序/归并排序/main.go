fea656f014e7ee9265b9ea2a0fa8d2c1d431fdc9
GuHang <499809640@qq.com>
2023-10-26 20:55:32 +0800

    递归，合并

---
+++ 十大经典排序/归并排序/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(MergeSort([]int{4, 3, 12, 5, 124, 465, 123, 53, 675, 12325, 15, 61, 56, 234, 2}))
+
+}
+
+func MergeSort(arr []int) []int {
+
+	if len(arr) < 2 {
+		return arr
+	}
+
+	left := MergeSort(arr[:len(arr)/2])
+	right := MergeSort(arr[len(arr)/2:])
+	return Merge(left, right)
+
+}
+
+func Merge(left, right []int) []int {
+	res := make([]int, len(left)+len(right))
+	i, j := 0, 0
+	m := 0
+	for i < len(left) || j < len(right) {
+		if i == len(left) {
+			res = append(res[0:m], right[j:]...)
+			break
+		}
+		if j == len(right) {
+			res = append(res[0:m], left[i:]...)
+			i++
+			break
+		}
+		if left[i] <= right[j] {
+			res[m] = left[i]
+			i++
+		} else {
+			res[m] = right[j]
+
+			j++
+		}
+		m++
+	}
+	return res
+}
